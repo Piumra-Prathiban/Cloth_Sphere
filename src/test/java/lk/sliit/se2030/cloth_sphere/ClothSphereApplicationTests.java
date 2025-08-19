ee9333ce02c28c080c22717d75b19424a15c715f
IT24100140 <piumrawork@gmail.com>
2025-08-19 21:59:59 +0530

    file initilaization

---
+++ src/test/java/lk/sliit/se2030/cloth_sphere/ClothSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.se2030.cloth_sphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
