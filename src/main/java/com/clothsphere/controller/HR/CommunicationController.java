c1b0aa4576bb85b3e8ad638227dab7f2938306e6
IT24100446 <IT24100446@my.sliit.lk>
2025-10-07 23:18:55 +0530

    SOM 10

--- src/main/java/com/clothsphere/controller/HR/CommunicationController.java
+++ src/main/java/com/clothsphere/controller/HR/CommunicationController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpSession;
-import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Optional;
@@ -18,6 +17,8 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/communication")
 public class CommunicationController {
+
+
 
     @Autowired
     private CommunicationService communicationService;
@@ -57,8 +58,9 @@ public class CommunicationController {
     /**
      * Send message using service layer
      */
-    @PostMapping("/send")
+    //@PostMapping("/send")
     @ResponseBody
+
     public ResponseEntity<Map<String, Object>> sendMessage(
             @RequestParam String senderEmail,
             @RequestParam String receiverEmail,
